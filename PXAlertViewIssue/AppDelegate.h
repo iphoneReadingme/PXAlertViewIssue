413da92ea33eed0baf9a64af8876303aaea213b6
zhoujie903 <zhoujie_903@163com>
2013-11-12 09:39:58 +0800

    Initial Commit

---
+++ PXAlertViewIssue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PXAlertViewIssue
+//
+//  Created by apple on 13-11-12.
+//  Copyright (c) 2013年 uniview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PXAlertViewIssue/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PXAlertViewIssue
+//
+//  Created by apple on 13-11-12.
+//  Copyright (c) 2013年 uniview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
